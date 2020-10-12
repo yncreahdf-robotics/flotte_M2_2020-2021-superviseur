988b6932751fab61ca51bd725c903cdb89174fba
CorentinStauder <71428868+CorentinStauder@users.noreply.github.com>
2020-10-12 15:49:18 +0200

    Update ros_entrypoint.sh

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -14,15 +14,15 @@ source "/opt/ros/$ROS_DISTRO/setup.bash"
 
 # setup workspace if it exists
 if [ -n "$WORKSPACE_NAME" ]; then 
-    if [ ! -e "/root/$WORKSPACE_NAME/catkin_ws/devel/setup.sh" ]; then
+    if [ ! -e "/home/$WORKSPACE_NAME/catkin_ws/devel/setup.sh" ]; then
         previousDirectory=$(pwd)
-        cd /root/$WORKSPACE_NAME
-        mkdir /root/$WORKSPACE_NAME/catkin_ws/src
-        cd /root/$WORKSPACE_NAME/catkin_ws/
+        cd /home/$WORKSPACE_NAME
+        mkdir /home/$WORKSPACE_NAME/catkin_ws/src
+        cd /home/$WORKSPACE_NAME/catkin_ws/
         catkin_make
         cd $previousDirectory
     fi
-    source "/root/$WORKSPACE_NAME/catkin_ws/devel/setup.sh"
+    source "/home/$WORKSPACE_NAME/catkin_ws/devel/setup.sh"
 fi
 
 exec "$@"
