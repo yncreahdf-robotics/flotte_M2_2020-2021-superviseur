ba99b11b4021778baa0c5afaccb5e699e4c143f8
ThomasDegallaix <thomas.degallaix@isen.yncrea.fr>
2020-10-15 16:56:56 +0200

    Ajout description fonctionnement principal

--- workspace/IHM.py
+++ workspace/IHM.py
@@ -7,68 +7,13 @@ import curses
 import subprocess
 import os
 import time
-<<<<<<< HEAD
-#import CommandeSQL
-=======
->>>>>>> e8b942cb04c6ef5db4343a39178e8f54eda98d66
 
 
-"""
-#Fonction to modify the IP at lines ROS_MASTER_URI et ROS_HOSTNAME du .zshrc
-def modifIP(stdscr):
-    stdscr.clear()	#clear the screen
-    curses.echo()  #prints what's typed on the keyboard
-    stdscr.addstr("Adresse IP Superviseur ? (Enter to continue, NO SPACES)\nExample : 10.224.0.52\n")
-    ip_sup = stdscr.getstr() #Retrieves supervisor's IP
-    stdscr.clear()	#clear screen
-    stdscr.addstr("Adresse IP Robot ? (Enter to continue, NO SPACES)\nExample : 10.224.0.51\n")
-    ip_robot = stdscr.getstr() #Retrieves Robot's IP
-    curses.noecho() #unables character printing on the screen
 
-    file = open("/home/nvidia/.zshrc", "r")	# "r"=reading 
-    content=file.readlines()
-    file.close()
-    for i in range(len(content)):
-        if ("ROS_MASTER") in content[i]:
-            content[i] = "export ROS_MASTER_URI=http://"+ip_sup+":11311"
-        if ("ROS_HOSTNAME") in content[i]:
-            content[i] = "\nexport ROS_HOSTNAME="+ip_robot+"\n"
+menu_accueil = ["Customer","Employee"]
+menu_customer= ["Command","Call Waiter","Exit"]
+menu_employee= ["Add New Robot","Follow Me","Start Service","Exit"]
 
-    file2 = open("/home/nvidia/.zshrc", "w")
-    for line in content:
-        file2.write(line)
-    file2.close()
-    """
-
-def modifIP_sup(stdscr):
-	stdscr.clear()	#	clear the screen
-	curses.echo() 	#	enable to print what's typed on keyboard
-	stdscr.addstr("Enter the supervisors IP \n (No spaces allowed) \n Press Enter to continue")	#	print to the screen
-	ip_sup=stdscr.getstr()	#	Retrieves supervisor's IP
-	file=open("/path","r")		#	Open the file at the given path
-	content=file.readlines()	#	Reads file until its end
-	for i in range(len(content)):
-		if("ROS_MASTER") in content [i]:
-			content [i] = "export ROS_MASTER_URI=http://"+ip_supp+":11311"
-	file2 = open("/path","r")
-	for line in content:
-		file2.write(line)
-	file2.close()
-
-def modifIP_robot(stdscr):
-	stdscr.clear()	#	clear the screen
-	curses.echo() 	#	enable to print what's typed on keyboard
-	stdscr.addstr("Enter the robot /add id/ IP \n (No spaces allowed) \n Press Enter to continue")	#	print to the screen
-	ip_robot=stdscr.getstr()	#	Retrieves supervisor's IP
-	file=open("/path","r")		#	Open the file at the given path
-	content=file.readlines()	#	Reads file until its end
-	for i in range(len(content)):
-		if("ROS_HOSTNAME") in content [i]:	#	!!! need to add an id verification to modify only the ID of the right robot
-			content [i] = "export ROS_MASTER_URI=http://"+ip_supp+":11311"
-	file2 = open("/path","r")
-	for line in content:
-		file2.write(line)
-	file2.close()
 
 def print_menu(stdscr, selected_row_idx, menu):
 	stdscr.clear()
@@ -87,15 +32,6 @@ def print_menu(stdscr, selected_row_idx, menu):
 	stdscr.refresh()	#	Affiche les derniers changements apportés
 
 
-#changes the environment variables for  ROS_MASTER_URI and ROS_HOSTNAME
-'''def set_environment(master, ip):
-	os.environ['ROS_MASTER_URI']=master
-	os.environ['ROS_HOSTNAME']=ip
-
-#Retrieves the environment variables for ROS_MASTER_URI and ROS_HOSTNAME
-def get_environment():
-	return os.environ['ROS_MASTER_URI'], os.environ['ROS_HOSTNAME']
-'''
 #Prints text centered
 def print_center(stdscr,text):
 	stdscr.clear()
@@ -105,18 +41,6 @@ def print_center(stdscr,text):
 	stdscr.addstr(y,x,text)
 	stdscr.refresh()
 
-#Launches
-'''def start_launch(launch_name,stdscr):
-	stdscr.nodelay(1)
-	uuid=roslaunch.rlutil.get_or_generate_uuid(None,True)
-	roslaunch.configure_logging(uuid)
-	launch=roslaunch.parent.ROSLaunchParent(uuid,['path'])
-	launch.start()	#start ros launch
-	while stdscr.getch() != 27: # 27=ESC
-		pass
-	launch.shutdown()	#	ends launch
-'''
-<<<<<<< HEAD
 
 def main(stdscr):
 	
@@ -186,5 +110,3 @@ def main(stdscr):
 
 curses.wrapper(main)
 
-=======
->>>>>>> e8b942cb04c6ef5db4343a39178e8f54eda98d66
--- workspace/Main.py
+++ workspace/Main.py
@@ -17,9 +17,27 @@
 import IHM
 
 
-# Import des librairies exterieurs au projet
+# Import des librairies exterieures au projet
 
-import curses
+# import curses
+
+
+
+
+######################
+### Initialisation ###
+######################
+
+
+# Lancement IHM Initialisation
+
+# Mode ecoute sur le topic de scan des nouveaux robots
+
+# Si nouveau robot
+
+	# Récupération des infos du message
+
+	# Mise en BDD
 
 
 
@@ -29,90 +47,31 @@ import curses
 ############
 
 
-# Menus de l'interface
+# Lancement de L'IHM principale
 
-menu_accueil = ["Customer","Employee"]
-menu_customer= ["Command","Call Waiter","Exit"]
-menu_employee= ["Add New Robot","Follow Me","Start Service","Exit"]
+IHM
 
 
-# Boucle principale
+# Ecoute des nouveaux clients
 
-def main(stdscr):
-	
-	menu = menu_accueil
+# Si nouveau client
 
-	#turn off cursor blinking
-	curses.curs_set(0)
+	# Si robot serveur dispo
 
-	#color scheme for selected row
-	curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
+		# Message vers le robot pour emmener le/les clients a la table
 
-	#specify the current selected row
-	current_row = 0
+# Ecoute des nouvelles commandes
 
+# Si nouvelle commande
 
-	while 1:
+	# Si preparateur dispo
 
-		#print the menu
-		IHM.print_menu(stdscr, current_row, menu)
+		# Message pour preparer la commande
 
-		key = stdscr.getch()
+# Ecoute des commandes a livrer
 
-		# Monte si la flèche du haut est pressée
-		if key == curses.KEY_UP and current_row>0:
-			current_row -= 1
+# Si commande a livrer
 
-		elif key == curses.KEY_DOWN and current_row < len(menu)-1:
-			current_row +=1
+	# Si robot serveur dispo
 
-		elif key == 27:
-			break
-
-		elif key in [10,13]:
-
-			stdscr.clear()
-			stdscr.refresh()
-
-		# Menu Accueil
-			if menu[current_row] == "Customer":
-				menu = menu_customer
-				current_row=0
-
-			elif menu[current_row] == "Employee":
-				menu = menu_employee
-				current_row=0
-
-		# Menu Customer
-			elif menu[current_row] == "Command":
-				#actions de commande
-				pass
-
-			elif menu[current_row] == "Call Waiter":
-				#appel de serveur humain
-				pass
-
-		# Menu Employee
-			elif menu[current_row] == "Add New Robot":
-				#ajout de robot à la flotte
-				pass
-
-			elif menu[current_row] == "Follow Me":
-				#mode suivi de personne
-				pass
-
-			elif menu[current_row] == "Start Service":
-				#démarrage fonctions robot
-				pass
-
-		# Commun Menus
-			elif menu[current_row] == "Exit":
-				menu = menu_accueil
-				current_row=0
-
-
-
-# Lancement de l'interface homme machine
-curses.wrapper(main)
-
-
+		# Envoi message pour livrer la commande
