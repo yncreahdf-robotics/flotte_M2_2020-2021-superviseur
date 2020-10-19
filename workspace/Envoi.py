97d42f74f2d81a2fb67e20b96eec3e993a6502e3
HeronISEN <heronisen@gmail.com>
2020-10-19 17:08:38 +0200

    Fonction fonctionnelles dans mqttperso.py

--- workspace/Envoi.py
+++ workspace/Envoi.py
@@ -1,41 +1,14 @@
 #!/usr/bin/env python3
 
-import paho.mqtt.client as mqtt
 import time
-# This is the Publisher
+
+import mqttperso
+
+ip = "192.168.1.5"
+
+mqttperso.publish(ip, 1883, "topiCoco", "testmessage", 2)
 
 
-def on_connect(client, userdata, flags, rc):
-
-	print("Connected with result code "+str(rc))
-	if rc==0:
-		print("ok")
-	else:
-		print("no")
-
-
-def on_disconnect(client, userdata, rc):
-
-	print("Client Got Disconnected")
-
-def on_message(client, userdata, msg):
-
-	print("Yes! i receive the message :" , str(msg.payload))
-	print("message received " ,str(msg.payload.decode("utf-8")))
-	print("message topic=",msg.topic)
-	print("message qos=",msg.qos)
-	print("message retain flag=",msg.retain)
-
-
-if __name__ == '__main__':
-
-
-	client = mqtt.Client()
-	client.connect("192.168.1.3",1883,60)
-	client.on_connect = on_connect
-	client.on_message = on_message
- 	client.on_disconnect = on_disconnect
-	
-	#client.publish("topic/InitialisationRobot", "1", qos=0)
-	client.publish("topic/TopiCoco", "1", qos=1)
-
+while (1):
+	time.sleep(5)
+	print("ok")
--- workspace/Reception.py
+++ workspace/Reception.py
@@ -1,44 +1,15 @@
 
 #!/usr/bin/env python
 
-import paho.mqtt.client as mqtt
-import paho.mqtt.subscribe as subscribe
+import time
 
-# Callbacks
+import mqttperso
 
-def on_connect(client, userdata, flags, rc):
+ip = "192.168.1.5"
 
-	print("Connected with result code "+str(rc))
-	if rc==0:
-		print("ok")
-	else:
-		print("no")
-	#client.subscribe("topic/DonneesRobots", qos=0)
-	client.subscribe("topic/TopiCoco", qos=1)
-
-def on_disconnect(client, userdata, rc):
-
-	print("Client Got Disconnected")
-
-def on_message(client, userdata, msg):
-
-	print("Yes! i receive the message :" , str(msg.payload))
-	print("message received " ,str(msg.payload.decode("utf-8")))
-	print("message topic=",msg.topic)
-	print("message qos=",msg.qos)
-	print("message retain flag=",msg.retain)
+mqttperso.subscribe(ip, 1883, "topiCoco", 2)
 
 
-if __name__ == '__main__':
-
-
-	client = mqtt.Client()	
-	client.connect("192.168.1.3",1883,60)
-	client.on_connect = on_connect
-	client.on_message = on_message
- 	client.on_disconnect = on_disconnect
-
-	client.loop_forever()
-
-	while (1):
-		print("ok")
+while (1):
+	time.sleep(5)
+	print("ok")
