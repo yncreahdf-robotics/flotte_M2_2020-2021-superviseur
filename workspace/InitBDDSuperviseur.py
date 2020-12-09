7b601c41cbe5d358e2750499569680a26fb95cf2
ThomasDegallaix <thomas.degallaix@isen.yncrea.fr>
2020-12-09 16:20:13 +0100

    Correction de plantage: fermeture des curseurs mysql à la fin de chaque requete

--- workspace/InitBDDSuperviseur.py
+++ workspace/InitBDDSuperviseur.py
@@ -11,22 +11,26 @@ def create_flotte_db(flotte_db):
 	mycursor=flotte_db.cursor()	
 	mycursor.execute("CREATE DATABASE IF NOT EXISTS flotte_db")
 	mycursor.execute("USE flotte_db")
+	mycursor.close()
 
 #	Check if database exists
-def check_flotte_db(mycursor):
+def check_flotte_db(flotte_db):
+	mycursor=flotte_db.cursor()
 	mycursor.execute("SHOW DATABASES")
 	for x in mycursor:
 		print(x)
+	mycursor.close()
 
 #	CREATE ALL TABLES
 def create_all_tables(flotte_db):
 	mycursor=flotte_db.cursor()
-	Article.create_Article_tb(mycursor)
-	Commande.create_Commande_tb(mycursor)
-	Table.create_Table_tb(mycursor)
-	Type.create_Type_tb(mycursor)
-	Robot.create_Robot_tb(mycursor)
-	Positions.create_Pose_tb(mycursor)
+	Article.create_Article_tb(flotte_db)
+	Commande.create_Commande_tb(flotte_db)
+	Table.create_Table_tb(flotte_db)
+	Type.create_Type_tb(flotte_db)
+	Robot.create_Robot_tb(flotte_db)
+	Positions.create_Pose_tb(flotte_db)
+	mycursor.close()
 
 #	DELETE DATABASE
 def delete_flotte_db(flotte_db):
@@ -35,7 +39,9 @@ def delete_flotte_db(flotte_db):
 	mycursor.execute(sql)
 	flotte_db.commit()
 	print("BDD:     ", mycursor.rowcount,"DATABASE DESTROYED **explosion**")
+	mycursor.close()
 
 def use_db(flotte_db):
 	mycursor=flotte_db.cursor()
 	mycursor.execute("USE flotte_db")
+	mycursor.close()
