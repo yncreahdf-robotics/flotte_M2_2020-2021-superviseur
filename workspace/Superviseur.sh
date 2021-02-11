f71a1d34d0a1fe7237e239c4814e5dad3774e1b9
ThomasDegallaix <thomas.degallaix@isen.yncrea.fr>
2021-02-11 13:06:47 +0100

    Ajout de Superviseur_setup.sh qui permet d'installer le superviseur ou de le mettre à jour

--- workspace/Superviseur.sh
+++ workspace/Superviseur.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 sudo service mysql stop
 sudo service mosquitto stop
 sudo docker-compose up -d && sudo docker-compose exec superviseur python Main.py sh
---
+++ workspace/Superviseur_setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.28.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo docker-compose --version
+
+
+sudo service mysql stop
+sudo service mosquitto stop
+sudo docker-compose up -d && sudo docker-compose exec superviseur python Main.py sh
